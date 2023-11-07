ab20f6ee87f35874f81dcfdf4a193276fd460bfb
iiuhenio <iuheniomail@yandex.ru>
2023-11-07 23:58:35 +0300

    6. ResponseEntity [#504799]

--- src/main/java/ru/job4j/persons/controller/PersonController.java
+++ src/main/java/ru/job4j/persons/controller/PersonController.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.persons.domain.Person;
@@ -16,8 +18,11 @@ import ru.job4j.persons.service.PersonService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.hibernate.tool.schema.SchemaToolingLogging.LOGGER;
@@ -79,6 +84,58 @@ public class PersonController {
             return ResponseEntity.ok().build();
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person is not deleted");
+    }
+
+    @GetMapping("/example1")
+    public ResponseEntity<?> example1() {
+        return ResponseEntity.ok("Example1");
+    }
+
+    @GetMapping("/example2")
+    public ResponseEntity<Map<String, String>> example2() {
+        return ResponseEntity.of(Optional.of(new HashMap<>() {{
+            put("key", "value");
+        }}));
+    }
+
+    @GetMapping("/example3")
+    public ResponseEntity<?> example3() {
+        Object body = new HashMap<>() {{
+            put("key", "value");
+        }};
+        var entity = new ResponseEntity(
+                body,
+                new MultiValueMapAdapter<>(Map.of("Job4jCustomHeader", List.of("job4j"))),
+                HttpStatus.OK
+        );
+        return entity;
+    }
+
+    @GetMapping("/example4")
+    public ResponseEntity<String> example4() {
+        var body = new HashMap<>() {{
+            put("key", "value");
+        }}.toString();
+        var entity = ResponseEntity.status(HttpStatus.CONFLICT)
+                .header("Job4jCustomHeader", "job4j")
+                .contentType(MediaType.TEXT_PLAIN)
+                .contentLength(body.length())
+                .body(body);
+        return entity;
+    }
+
+    @GetMapping("/example5")
+    public ResponseEntity<byte[]> example5() throws IOException {
+        var content = Files.readAllBytes(Path.of("./book.pdf"));
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .contentLength(content.length)
+                .body(content);
+    }
+
+    @GetMapping("/example6")
+    public byte[] example6() throws IOException {
+        return Files.readAllBytes(Path.of("./pom.xml"));
     }
 
     @ExceptionHandler(value = { IllegalArgumentException.class })
